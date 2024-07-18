4f0946e0146316ce347bfcde778fc3d27b1a5f44
sunkarabhargava <sunkarabhargava@gmail.com>
2024-07-18 19:35:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const main = () => {
     const id = uuidv4();
-    console.log(`Generated UUID: ${id}`);
+    console.log(`SESSION_ID: ${id}`);
 }
 
 main();
