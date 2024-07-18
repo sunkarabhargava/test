1e769e8c524facd1fd85adf21cc6d810c74d442d
sunkarabhargava <sunkarabhargava@gmail.com>
2024-07-18 19:30:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const generateId = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const r = Math.random() * 16 | 0;
-        const v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+const { v4: uuidv4 } = require('uuid');
 
 const main = () => {
-    const id = generateId();
+    const id = uuidv4();
     console.log(`Generated UUID: ${id}`);
 }
 
