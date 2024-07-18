d9c63424651bcf531f243559ac7e76d27a8aeaea
sunkarabhargava <sunkarabhargava@gmail.com>
2024-07-18 19:42:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
+const { execSync } = require('child_process');
 
 const main = () => {
     const id = uuidv4();
     console.log(`SESSION_ID: ${id}`);
+    execSync(`echo "ID=${id}" >> $GITHUB_ENV`);
 }
 
 main();
